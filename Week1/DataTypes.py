696fa017cbce1f1231a8b07c72b40ddb0ab52ca4
Liam Crowe <20093470@mail.wit.ie>
2024-02-08 23:25:15 +0000

    List of all the different data types which print to the console.

---
+++ Week1/DataTypes.py
@@ -0,0 +1,21 @@
+str = "Hello World"
+wholeNumber = 100
+decimalNumber = 100.5
+complexNumber = 100j
+lists = ["apple", "banana", "cherry"]
+tuples = ("apple", "banana", "cherry")
+renges = range(5)
+dictionary = {"name":"Liam","age": "22"}
+sets = {"apple", "banana", "cherry"}
+frozensets = frozenset({"apple", "banana", "cherry"})
+booleans = True
+bytes_hello = b"Hello"
+array_byte = bytearray(5)
+memView = memoryview(bytes(5))
+nothing = None
+
+print(str, wholeNumber, decimalNumber, complexNumber)
+print(lists, "//", tuples)
+print(renges)
+print(dictionary, "//", sets, "//", frozensets)
+print(booleans, "//", bytes_hello, "//", array_byte, "//", memView, "//", nothing)
