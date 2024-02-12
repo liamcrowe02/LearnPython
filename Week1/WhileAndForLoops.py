1c8eda980b5810e2780bc66f1e45b787047a41c3
Liam Crowe <20093470@mail.wit.ie>
2024-02-12 18:56:21 +0000

    'While' and 'for' loops covered along with keywords 'break' and 'continue'

---
+++ Week1/WhileAndForLoops.py
@@ -0,0 +1,21 @@
+#For loop
+fruit = ["apple", "banana", "cherry"]
+for x in fruit:
+    print(fruit)
+    if x == "banana":
+        break
+
+#While
+i = 0
+while i < 6:
+    i += 1
+    if i == 6:
+        break
+    print(i)
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
